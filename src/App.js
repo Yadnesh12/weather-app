7c7fd19bd3d9139f47ee0cb3ed3479d8c3dd7fd2
Yadnesh12 <yadneshbharambe123@gmail.com>
2024-03-09 12:01:16 +0530

    Added tailwind to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
   return (
     <div className="App">
-     hiiiiiiiiiiiii
+    <h1 className='text-2xl font-bold text-red-500'>hiiiiiiiiiiiii</h1> 
+    <UilReact size={50} />
     </div>
   );
 }
